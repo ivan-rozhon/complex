969ff5736b64382b039d2ba1ae008b878d25c3af
Rozhoň Ivan <irozhon@trask.cz>
2016-09-30 17:21:25 +0200

    web-schema rebuilded, work on router

--- _core/shared/shared-router.php
+++ _core/shared/shared-router.php
@@ -12,21 +12,23 @@ class SharedRouter {
     public function sharedRouter() {
         // return count($this->url_query_arr);
         // return $this->url_query_arr[0];
-        return implode(" ", $this->url_query_arr);
+        // return implode(" ", $this->url_query_arr);
         // return $this->sharedRouterConfig['sharedRouterConfig']['mainSchema'];
-        
+        // return $this->sharedRouterConfig['sharedRouterConfig']['mainSchema']['home']['template'];
+        // return $this->system->webSchema['webSchema']['webSchemaMain']['home']['template'];
         // TO DO: querySchema, depth, queryObject
 
         foreach($this->system->webSchema['webSchema'] as $schema) {
-            // $this->sharedRouterSearchQuery($schema);
-            foreach($schema as $query) {
-                
-            }
+            return $this->sharedRouterSearchQuery($schema);
+
+            // if (array_key_exists($this->url_query_arr[0], $schema)) {
+            //     return $this->url_query_arr[0];
+            // }
         }
     }
 
-    public function sharedRouterSearchQuery() {
-        
+    public function sharedRouterSearchQuery($schema) {
+        // vrátit array queryies (za sebou jdoucích parent-> child), nebo vrátit 'home'/'nenalezeno'
     }
 
     public function sharedRouterBaseUrl() {
