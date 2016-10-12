7f1d93a7c0af17f28e92ea54a40bcf4a084270ea
ivan-rozhon <ivan.rozhon@email.cz>
2016-10-12 23:16:01 +0200

    adminApp, json api

--- _core/admin/adminApp/adminApp.js
+++ _core/admin/adminApp/adminApp.js
@@ -3,14 +3,29 @@
     function AdminAppController($http) {
         var ctrl = this;
 
-        ctrl.data = {};
+        ctrl.status = null;
+        ctrl.data = {};    
 
         ctrl.getJson = function () {
             $http({
                 method: "GET",
                 url: "?api"
             }).then(function (response) {
+                ctrl.status = response.status;
                 ctrl.data = response.data;
+            });
+        };
+
+        ctrl.postJson = function () {
+            var json = JSON.stringify(ctrl.data);
+            // console.log(json);
+            $http({
+                method: "POST",
+                url: "?api",
+                data: { 'json' : json }
+            }).then(function (response) {
+                console.log(response.status);
+                console.log(response.data);
             });
         };
     }
--- _core/api/api.php
+++ _core/api/api.php
@@ -9,14 +9,23 @@ class Api {
     }
 
     public function api() {
-        // echo 'hello';
         // echo filter_input(INPUT_GET, 'action');
         // echo $this->requestMethod;
-        
+        // TO DO
+        // - metadata (depth, path, variables, etc.) - to identify json
         switch($this->requestMethod) {
             case 'GET':
                 // echo file_get_contents('_source/_data/data-about.json');
-                echo file_get_contents('_source/web-schema.json');
+                echo file_get_contents('_source/_data/data-contact.json');
+                break;
+            case 'POST':
+                var_dump($_POST);
+                $post = json_decode(file_get_contents('php://input'),true);
+                var_dump($post);
+                $json_string = $post['json'];
+                // echo $input['json'];
+                // echo $input['action'];
+                file_put_contents('_source/_data/data-contact.json', $json_string);
                 break;
         }
     }
