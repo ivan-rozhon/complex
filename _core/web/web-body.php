4df957c2edbd68587479b06704bb90a89eabde67
Rozhoň Ivan <irozhon@trask.cz>
2016-10-20 17:34:47 +0200

    schema structure, scripts

--- _core/web/web-body.php
+++ _core/web/web-body.php
@@ -10,14 +10,19 @@ class WebBody {
     public function webBody($webBodyHeader, $webBodyNav, $webBodyMain, $webBodyFooter) {
         return '
             <body>
+                <!-- jQuery -->
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+                
+                <!-- Bootstrap core JavaScript -->
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+                <!-- JS -->
+                <script src="_scripts/web.js?v='.filemtime("_scripts/web.js").'"></script>
+                
                 '.$webBodyHeader.'
                 '.$webBodyNav.'
                 '.$webBodyMain.'
-                '.$webBodyFooter.'
-                <!-- jQuery -->
-                <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-                <!-- Bootstrap core JavaScript -->
-                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+                '.$webBodyFooter.'                
             </body>
         ';
     }
