e7bf18ca3e2b3e655a91de9aa0a85834c06548f0
ivan-rozhon <ivan.rozhon@email.cz>
2017-03-19 23:13:05 +0100

    editor - titles, colors

--- src/_core/admin/admin-app/shared/editor/editor.component.js
+++ src/_core/admin/admin-app/shared/editor/editor.component.js
@@ -4,70 +4,99 @@
     angular.module('shared.editorComponent', [])
         .component('npcEditor', {
             templateUrl: 'shared/editor/editor.component.html',
-            controller: EditorController
+            controller: EditorController,
+            bindings: {
+
+            }
         });
 
-    EditorController.$inject = [];
-    function EditorController() {
+    EditorController.$inject = ['$mdDialog', '$timeout'];
+    function EditorController($mdDialog, $timeout) {
         let $ctrl = this;
 
-        // display: block;
-        // position: absolute;
-        // margin - top: 8px;
-        // background - color: rgb(224, 224, 224);
-        // padding: 16px 8px;
+        // Current selected text attributes
+        $ctrl.currentAttributes = {};
+        // Titles (headings)
+        $ctrl.titles = [1, 2, 3, 4, 5, 6];
 
-        $ctrl.trix = '<div><!--block--><a href="http://sachinchoolur.github.io/angular-trix/">hello</a></div><ul><li><!--block-->lol<ul><li><!--block-->bum</li></ul></li></ul><div><!--block--><del>hey</del></div>';
+        // DEBUG
+        $ctrl.trix = '<div><!--block--><a href="http://sachinchoolur.github.io/angular-trix/">trix editor</a><br><br></div><pre><!--block-->&lt;code awesome="true"&gt;&lt;/code&gt;</pre><div><!--block--><br></div><blockquote><!--block-->Quoted <em>text italic<br></em>and<strong> bold</strong></blockquote><ul><li><h3><!--block-->T3</h3><ul><li><!--block-->2nd level <u>bullet</u></li></ul></li></ul><div><!--block--><del>strikethrough text</del></div>';
+        $ctrl.colors = ['F44336', '3F51B5', 'CDDC39', '4CAF50', 'FFC107', '795548'];
 
-        // init trix variables
-        // $ctrl.trixEditorActive = false;
+        // Define "underline" format
+        Trix.config.textAttributes.underline = { tagName: 'u' };
 
-        // Define "underline"" format
-        Trix.config.textAttributes.underline = {
-            tagName: 'u',
-            inheritable: true
-        };
+        // Define title`s formats
+        angular.forEach($ctrl.titles, function (value, key) {
+            Trix.config.blockAttributes[`title${value}`] = {
+                tagName: 'h' + value, breakOnReturn: true, group: false
+            };
+        });
 
-        $ctrl.underline = function () {
-            $ctrl.editor.setSelectedRange([3, 5]);
-            $ctrl.editor.deactivateAttribute("underline");
-        };
+        // Define colors formats
+        angular.forEach($ctrl.colors, function (value, key) {
+            Trix.config.textAttributes[`color${value}`] = { style: { 'color': '#' + value }, nestable: false };
+        });
 
         console.log(Trix.config);
-
-        // Trix.config.textAttributes.heading = { inheritable: true, tagName: 'h1' };
-        // Trix.config.textAttributes.subHeading = { inheritable: true, tagName: 'h2' };
 
         // trix-initialize (trix initialization)
         // @e trix event
         // @editor Trix.Editor instance
         $ctrl.trixInitialize = function (e, editor) {
-            // editor.insertString("Hello");
-            // editor.setSelectedRange([0, 5]);
-            // editor.activateAttribute("underline");
-            // $ctrl.editor = editor;
+            // After event post processing
+            $ctrl.afterEvent(e, editor);
         };
 
         // trix-change (text format changed)
         $ctrl.trixChange = function (e, editor) {
-            // console.log(arguments);
+            // After event post processing
+            $ctrl.afterEvent(e, editor);
         };
 
         // trix-selection-change
         $ctrl.trixSelectionChange = function (e, editor) {
-            // console.log(arguments);
+            // After event post processing
+            $ctrl.afterEvent(e, editor);
         };
 
         // trix-focus (if <trix-editor> is focused)
         $ctrl.trixFocus = function (e, editor) {
             // add 'active' class
             editor.element.classList.add('active');
+
+            // After event post processing
+            $ctrl.afterEvent(e, editor);
         };
 
         // trix-blur (if <trix-editor> is blured)
         $ctrl.trixBlur = function (e, editor) {
             // remove 'active' class
             editor.element.classList.remove('active');
+
+            // After event post processing
+            $ctrl.afterEvent(e, editor);
+        };
+
+        // After event (user interact) actions
+        $ctrl.afterEvent = function (e, editor) {
+            // timeout fix
+            $timeout(function () {
+                // save current selected text attributes
+                $ctrl.currentAttributes = angular.copy(editor.composition.currentAttributes);
+            }, 200);
+        };
+
+        // Editor attribute active flag check
+        $ctrl.isAttrActive = function (attrName, arrayName) {
+            let state = false;
+
+            // iterate over defined attributes
+            for (let i of $ctrl[arrayName]) {
+                state = $ctrl.currentAttributes[`${attrName}${i}`] ? true : state;
+            }
+
+            return state;
         };
     }
 })();
