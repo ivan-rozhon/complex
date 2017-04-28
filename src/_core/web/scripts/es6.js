dcd9860887143dae1aed8f6681d50562b2d6b937
Ivan Rozhoň (ext97075) <ivan.rozhon@ext.csas.cz>
2017-04-28 16:38:03 +0200

    ES6 Destructuring & Template Strings

--- src/_core/web/scripts/es6.js
+++ src/_core/web/scripts/es6.js
@@ -8,12 +8,11 @@
     let evens = [1, 5, 10, 15],
         fives = [];
 
-    // Expression bodies
+    // ═ Expression bodies ═
     let odds = evens.map(v => v + 1);
     let nums = evens.map((v, i) => v + i);
     let pairs = evens.map(v => ({ even: v, odd: v + 1 }));
 
-    // #LOG
     console.log(`evens ${evens}; odds ${odds}; nums ${nums};`);
     console.log(pairs);
 
@@ -23,7 +22,6 @@
             fives.push(v);
     });
 
-    // #LOG
     console.log(`fives ${fives}`);
 
     // Lexical this
@@ -36,7 +34,6 @@
         }
     };
 
-    // #LOG
     bob.printFriends();
 
     // Lexical arguments
@@ -53,7 +50,6 @@
         return example();
     }
 
-    // #LOG
     console.log(square(2, 4, 7.5, 8, 11.5, 21));
 
     // « Classes »
@@ -65,7 +61,6 @@
         return x + y;
     }
 
-    // #LOG
     console.log(f(3));
 
     function f2(x, ...y) {
@@ -73,14 +68,56 @@
         return x * y.length;
     }
 
-    // #LOG
     console.log(f2(3, "hello", true));
 
     function f3(x, y, z) {
         return x + y + z;
     }
 
-    // #LOG
     // Pass each elem of array as argument
     console.log(f3(...[1, 2, 3]));
+
+    // « Destructuring & Template Strings » (Objects and Strings)
+    let name = 'Sam';
+    let age = 45;
+
+    // Object Initializer Shorthand
+    let user = { name, age };
+
+    console.log(`user name: ${user.name}`);
+    console.log(`user age: ${user.age}`);
+
+    function buildUser(first, last, postCount) {
+        // Template Strings
+        let fullName = `${first} ${last}`;
+        const ACTIVE_POST_COUNT = 10;
+
+        return {
+            first,
+            last,
+            fullName,
+            // Method Initializer Shorthand
+            isActive() {
+                return postCount >= ACTIVE_POST_COUNT;
+            }
+        };
+    }
+
+    // Object destructuring
+    let { first, fullName, isActive } = buildUser('Sam', 'Williams', 20);
+
+    console.log(first);
+    console.log(fullName);
+    console.log(isActive());
+
+    // Unescaped template strings
+    console.log(String.raw`In ES5 "\n" is a line-feed.`);
+
+    // Destructuring + defaults arguments
+    function r({ x, y, w = 10, h = 10 }) {
+        return x + y + w + h;
+    }
+
+    console.log(r({x:1, y:2}));
+
 })();
