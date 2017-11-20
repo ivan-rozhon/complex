b8b46a9f278fade0117080775d975cfcd5450985
Rozhoň Ivan <irozhon@trask.cz>
2017-11-20 17:00:38 +0100

    major changes: removed bower, gulp, admin 1; add webpack parallel build; general simplification

--- README.md
+++ README.md
@@ -3,38 +3,32 @@
 Experimental web dev pack, that contains:
 - PHP and Javascript (Angular) CMS
 - REST API
-- Local development environment (Gulp, Webpack)
+- Local development environment (Webpack, PHP Webserver)
 
 ## Install
 ```
 npm install
-bower install
 ```
 
-## Build
+## Build & watch
 Basic build & watch:
 ```
-gulp
-```
-or separately:
-```
-gulp build
-gulp watch
-```
-production:
-```
-gulp build -prod
+npm run build
 ```
 
 ## Local dev server
 (PHP ~7.1.0 required)
 
-After successful build:
+After successful build (in separate terminal):
 ```
-gulp connect
+npm run serve
 ```
 
-## News & Near future
-- Webpack 3
-- Angular 4 Admin App
-- Removing Bower (deprecated) & Gulp
+## News
+- Webpack 3 build
+- Angular 4 Admin App 2 in development (will replace Admin 1)
+- Removed: Bower (deprecated) & Gulp, Admin 1
+
+## Near future
+- Angular 5
+- UIkit for web and admin
--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,49 @@
-module.exports = require('./config/webpack.dev.js');
+var webpack = require('webpack');
+var webpackMerge = require('webpack-merge');
+var commonWebConfig = require('./config/webpack-web.common.js');
+var commonAdminConfig = require('./config/webpack-admin.common.js');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var helpers = require('./config/helpers');
+
+const ENV = process.env.NODE_ENV = process.env.ENV = 'production';
+
+module.exports = [
+    webpackMerge(commonWebConfig, {
+        output: {
+            path: helpers.root('dist'),
+            filename: '_core/web/scripts/[name].js',
+        },
+
+        plugins: []
+    }),
+    webpackMerge(commonAdminConfig, {
+        devtool: 'source-map',
+
+        output: {
+            path: helpers.root('dist/_core/admin2/admin-app'),
+            publicPath: '_core/admin2/admin-app/',
+            filename: '[name].js',
+            chunkFilename: '[id].chunk.js'
+        },
+
+        plugins: [
+            new webpack.NoEmitOnErrorsPlugin(),
+            new webpack.optimize.UglifyJsPlugin({ // https://github.com/angular/angular/issues/10618
+                mangle: {
+                    keep_fnames: true
+                }
+            }),
+            new ExtractTextPlugin('[name].css'),
+            new webpack.DefinePlugin({
+                'process.env': {
+                    'ENV': JSON.stringify(ENV)
+                }
+            }),
+            new webpack.LoaderOptionsPlugin({
+                htmlLoader: {
+                    minimize: false // workaround for ng2
+                }
+            })
+        ]
+    })
+];
