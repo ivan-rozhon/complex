0304ac3448c7a371ab3bc427a24c70e3f86462b6
Rozhoň Ivan <irozhon@trask.cz>
2018-02-23 23:10:49 +0100

    footer layout, navbar righ (icons)

--- src/_core/web/web-body-footer.php
+++ src/_core/web/web-body-footer.php
@@ -11,11 +11,13 @@ class WebBodyFooter {
     public function webBodyFooter() {
         $baseUrl = $this->webBody->web->sharedRouter->baseUrl();
         $basePath = $this->webBody->web->sharedRouter->basePath();
+        $logoVersion = filemtime("_source/media/shared/logo.svg");
 
         return $this->templateProvider->sharedTemplateProvider(
             [
                 'baseUrl' => $baseUrl,
-                'basePath' => $basePath
+                'basePath' => $basePath,
+                'logoVersion' => $logoVersion
             ]
             , '_core/web/web-body-footer.html');
     }
