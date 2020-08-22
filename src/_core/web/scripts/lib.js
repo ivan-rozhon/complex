89acc468083d57ff73378ce726e9c54290df6406
Ivan Rozhon <ivan.rozhon@gmail.com>
2020-08-22 22:12:21 +0200

    remove old sources

--- src/_core/web/scripts/lib.js
+++
@@ -1,8 +0,0 @@
-// JS 3rd party vendor libraries file (imported to vendor.ts)
-
-// UIKit | https://getuikit.com/
-import UIkit from 'uikit';
-import Icons from 'uikit/dist/js/uikit-icons';
-
-// loads the Icon plugin
-UIkit.use(Icons);
--- src/_core/web/styles/postcss.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    plugins: {
-        'postcss-import': {},
-        'postcss-cssnext': {},
-        // Disable autoprefixer, because it's already included in cssnext
-        'cssnano': { autoprefixer: false }
-    }
-};
