673c0b14be1de24154d0bea09da94baa4131c474
Ivan Rozhon <ivan.rozhon@gmail.com>
2020-08-23 15:33:03 +0200

    ts aliases

--- admin/src/app/app.module.ts
+++ admin/src/app/app.module.ts
@@ -4,18 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { SharedModule } from './shared/shared.module';
+import { SharedModule } from '@cx-shared/shared.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    SharedModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, SharedModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
