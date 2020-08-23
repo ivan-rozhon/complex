ea96e04b60dcc1f05f2dd76cf4740e4bc052d1a3
Ivan Rozhon <ivan.rozhon@gmail.com>
2020-08-23 15:28:48 +0200

    ui: layout component

---
+++ admin/src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LayoutComponent } from './components/layout/layout.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [LayoutComponent],
+  exports: [LayoutComponent],
+})
+export class SharedModule {}
